1008dd48f389ea2a0c33e59061c0ce4ae91356bc
mr-leo44 <lionnelkaniki44@gmail.com>
2025-04-05 03:11:20 +0100

    feat: add Waiter Model with migration

---
+++ database/migrations/2025_04_05_025851_create_waiters_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Models\LoungeExtension;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('waiters', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(LoungeExtension::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('waiters');
+    }
+};
