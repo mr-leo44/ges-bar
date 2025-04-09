7fb31d052d24d6b59ad85ea3b859af65edb11cf2
mr-leo44 <lionnelkaniki44@gmail.com>
2025-04-09 15:02:46 +0100

    feat: add 1-to-many relation between lounge and loungeAdmin

--- app/Models/Lounge.php
+++ app/Models/Lounge.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\LoungeAdmin;
 use App\Models\LoungeExtension;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -23,5 +24,10 @@ class Lounge extends Model
     public function lounge_extensions(): HasMany
     {
         return $this->hasMany(LoungeExtension::class);
+    }
+
+    public function lounge_admins(): HasMany
+    {
+        return $this->hasMany(LoungeAdmin::class);
     }
 }
--- app/Models/LoungeAdmin.php
+++ app/Models/LoungeAdmin.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use App\Models\Lounge;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LoungeAdmin extends Model
 {
-    protected $guarded= [];
+    protected $fillable = [
+        'lounge_id',
+    ];
+
+    public function lounge(): BelongsTo
+    {
+        return $this->belongsTo(Lounge::class);
+    }
 }
